655d3eb07b5afa1c490db5a4173fa92a55f1f3a2
fernando <dlu@kalengo.com>
2020-12-29 23:44:44 +0800

    feat: 确定技术,项目初始化,eslint,prettier配置

---
+++ README.md
@@ -0,0 +1,11 @@
+- [x] 安装 typescript
+
+- [x] eslint + prettier 项目风格统一
+
+- [x] 完善代码格式化，增加 .vscode/settings.json
+
+- [ ] 增加 babel-polyfill
+
+- [ ] 引入 axios
+
+- [ ] 增加 stylelint, commitlint
---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <img alt="aaa" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script lang="ts">
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
